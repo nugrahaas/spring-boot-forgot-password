b0e2139f8c388e612356b7649b55be1e39f562fb
Nugraha Adi Sulistyo <tyoku1792@gmail.com>
2022-04-20 02:27:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-boot-forgot-password
+ Forgot your password? Here's the right app
---
+++ src/main/java/com/nugrahaatyo/service/framework/EmailService.java
@@ -0,0 +1,16 @@
+package com.nugrahaatyo.service.framework;
+
+import com.nugrahaatyo.entity.Mail;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reset-password
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/04/22
+ * Time: 08.38
+ */
+public interface EmailService {
+    void send(Mail mail);
+}
